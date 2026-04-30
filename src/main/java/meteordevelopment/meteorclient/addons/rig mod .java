4d48a52bf597f8fae2756a52b24c0046d876270a
severusa12 <116972370+severusa12@users.noreply.github.com>
2026-04-30 23:24:50 +0400

    Update rig mod .java

--- src/main/java/meteordevelopment/meteorclient/addons/rig mod .java
+++ src/main/java/meteordevelopment/meteorclient/addons/rig mod .java
@@ -1,39 +1,27 @@
-package com.example.addon.modules;
+private final Setting<Boolean> enabled = sgGeneral.add(new BoolSetting.Builder()
+    .name("rig-gambling")
+    .defaultValue(true)
+    .build());
 
-import meteordevelopment.meteorclient.events.packets.PacketEvent;
-import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.settings.*;
-import meteordevelopment.orbit.EventHandler;
-import com.example.gambling.packets.GamblingResultPacket; 
+private final Setting<Integer> winAmount = sgGeneral.add(new IntSetting.Builder()
+    .name("win-amount")
+    .description("Amount to force.")
+    .defaultValue(1000)
+    .min(1)
+    .sliderMax(1000000)
+    .build());
 
-public class GamblingRig extends Module {
-    private final SettingGroup sgGeneral = settings.getDefaultGroup();
+public GamblingRig() {
+    super(com.example.addon.Addon.CATEGORY, "gambling-rig", "Forces wins.");
+}
 
-    private final Setting<Boolean> enabled = sgGeneral.add(new BoolSetting.Builder()
-        .name(\"rig-gambling\")
-        .defaultValue(true)
-        .build());
+@EventHandler
+private void onReceivePacket(PacketEvent.Receive event) {
+    if (!enabled.get()) return;
 
-    private final Setting<Integer> winAmount = sgGeneral.add(new IntSetting.Builder()
-        .name(\"win-amount\")
-        .description(\"Amount to force.\")
-        .defaultValue(1000)
-        .min(1)
-        .sliderMax(1000000)
-        .build());
-
-    public GamblingRig() {
-        super(com.example.addon.Addon.CATEGORY, \"gambling-rig\", \"Forces wins.\");
-    }
-
-    @EventHandler
-    private void onReceivePacket(PacketEvent.Receive event) {
-        if (!enabled.get()) return;
-
-        if (event.packet instanceof GamblingResultPacket) {
-            GamblingResultPacket packet = (GamblingResultPacket) event.packet;
-            packet.setWin(true);
-            packet.setAmount(winAmount.get());
-        }
+    if (event.packet instanceof GamblingResultPacket) {
+        GamblingResultPacket packet = (GamblingResultPacket) event.packet;
+        packet.setWin(true);
+        packet.setAmount(winAmount.get());
     }
 }
